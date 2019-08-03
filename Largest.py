6a5bbb47966357eb51db7321a00ac160fc3e011a
anitha231119 <53600754+anitha231119@users.noreply.github.com>
2019-08-03 16:20:17 +0530

    Create Largest.py

---
+++ Largest.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number:"))
+b=int(intput("enter the second number:))
+c=int(input("enter the third number:"))
+If a>b and a>c:
+   Print("a is greater:")
+Elif b>a and b>c:
+   Print("b is greater:")
+Else:
+   Print("c is greater:")
