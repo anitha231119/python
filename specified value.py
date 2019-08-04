58687657a2a3a91fc4dc9c6f04492feaf4905588
anitha231119 <53600754+anitha231119@users.noreply.github.com>
2019-08-04 20:52:31 +0530

    Create specified value.py

---
+++ specified value.py
@@ -0,0 +1,8 @@
+def is_group_member(group_data,n):
+    for value in group_data:
+        if n==value:
+           return true
+        else false
+Print(is_group_member([1,2,3,4,5],5))
+Print(is_group_member([1,3,7],0))
+
